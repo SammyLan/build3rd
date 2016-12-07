74a85caee0efe06a544ef8aec979475fdc904a1b
SammyLan <sammylan234@gmail.com>
2016-12-07 15:58:51 +0800

    通用脚本

---
+++ InitDir.bat
@@ -0,0 +1,6 @@
+@if not exist %LOGS_DIR% mkdir %LOGS_DIR%
+@if not exist %BULID_DIR% mkdir %BULID_DIR%
+@if not exist %LIB3rd% mkdir %LIB3rd%
+@if not exist %LIB3rd_Inc% mkdir %LIB3rd_Inc%
+@if not exist %LIB3rd_Bin%Debug mkdir %LIB3rd_Bin%Debug
+@if not exist %LIB3rd_Bin%Release mkdir %LIB3rd_Bin%Release
